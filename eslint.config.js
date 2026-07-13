54e11b7053115130952f69edd2197ae2b2178913
RahmadFahrurrozi <zfahrur150105@gmail.com>
2026-07-13 16:18:22 +0700

    chore: update package-lock.json and format eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 const typescriptEslint = require("@typescript-eslint/eslint-plugin");
 const typescriptParser = require("@typescript-eslint/parser");
 const prettierPlugin = require("eslint-plugin-prettier");
-const prettierConfig = require("eslint-config-prettier");
 
 module.exports = [
   {
